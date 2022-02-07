c9af820cbc7f630d488e3cc5128f860eb5289114
IronMan <2012joe@gmail.com>
2022-02-07 10:04:21 +0800

    create directory structure

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an app to find the latest DJ and other music event.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ pages/events/[slug].js
@@ -0,0 +1,3 @@
+export default function EventPage() {
+  return <h1>Single Event Page</h1>
+}
